d304daa2469ae39a95908106c00def96c56dc13c
NakamuraHayato <br343nh@yahoo.co.jp>
2020-05-17 14:50:44 +0900

    重力の設定と床オブジェクトの作成

--- main.js
+++ main.js
@@ -1,46 +1,66 @@
 enchant();
 
-window.onload = function() {
+window.onload = function () {
 
-  let core = new Core(320, 320);
-  core.preload('chara1.png');
-  core.fps = 15;
-  core.rootScene.backgroundColor = 'gray';
-  core.keybind(32, 'sp'); // スペースキーをspとして登録
+  let game = new Core(320, 320);
+  game.preload('chara1.png');
+  game.preload('map2.png');
+  game.rootScene.backgroundColor = 'gray';
+  game.keybind(32, 'sp'); // スペースキーをspとして登録
 
-  core.onload = function() {
+  game.onload = function () {
+    // 物理エンジン使用宣言
+    let world = new PhysicsWorld(0, 9.8);
+    game.rootScene.onenterframe = function () {
+      world.step(game.fps);
+    }
+
+    // 床オブジェクト
+    let Floor = Class.create(PhyBoxSprite, {
+      initialize: function (x, y) {
+
+        PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false);
+        this.image = game.assets['map2.png'];
+        this.frame = 0;
+        this.x = x;
+        this.y = y;
+        game.rootScene.addChild(this);
+      }
+    });
+
+    let floors = [];
+    for (let i = 0; i < 20; ++i) {
+      floors[i] = new Floor(i * 16, 200);
+    }
 
     // くまオブジェクト
-    let bear = new Sprite(32,32);
-    bear.image = core.assets['chara1.png'];
+    let bear = new PhyBoxSprite(32, 32, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.5, 0.3, true);
+    bear.image = game.assets['chara1.png'];
     bear.x = 10;
-    bear.y = 150;
+    bear.y = 160;
     bear.frame = 4;
 
     bear.on('enterframe', () => {
       bear.x += 5;
       // 入力に応じた移動
-      if (core.input.sp) {
-        bear.y -= 5;
+      if (game.input.sp) {
+        bear.applyImpulse(new b2Vec2(0, -1));
       }
 
       // スマホ用のタッチ操作
-      core.rootScene.on('touchstart', function() {
-        bear.y -= 1;
+      game.rootScene.on('touchstart', function () {
+        bear.applyImpulse(new b2Vec2(0, -1));
       });
 
       if (bear.x > 320) {
         bear.x = 0;
       }
-      if (bear.y < 0) {
-        bear.y = 320;
-      }
 
       // 敵との当たり判定(within)
       // if (this.within(enemy, 20)) {
       //   labelHit.text = "Hit!";
-      //   core.pushScene(gameOverScene);
-      //   core.stop();
+      //   game.pushScene(gameOverScene);
+      //   game.stop();
       // } else {
       //   labelHit.text = "remote";
       // }
@@ -58,7 +78,7 @@ window.onload = function() {
     labelTime.font = '14px Arial';
     labelTime.text = 'remote';
     labelTime.on('enterframe', () => {
-      labelTime.text = (core.frame / core.fps).toFixed(2) ;
+      labelTime.text = (game.frame / game.fps).toFixed(2);
     });
 
     // 当たり判定を表示するラベル
@@ -69,11 +89,11 @@ window.onload = function() {
     labelHit.font = '14px Arial';
     labelHit.text = 'hit';
 
-    core.rootScene.addChild(bear);
-    core.rootScene.addChild(labelTime);
-    core.rootScene.addChild(labelHit);
+    game.rootScene.addChild(bear);
+    game.rootScene.addChild(labelTime);
+    game.rootScene.addChild(labelHit);
   }
-  core.start();
+  game.start();
 };
 
 // 0~nまでのランダムな整数を生成
