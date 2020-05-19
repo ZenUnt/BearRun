c2dbef0f8c1942146c5e707a85ca77dd42d6549e
NakamuraHayato <br343nh@yahoo.co.jp>
2020-05-19 22:59:39 +0900

    走行から飛行に変更、床をボムに変更

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ window.onload = function () {
   let game = new Core(640, 320);
   game.preload('chara1.png');
   game.preload('map2.png');
+  game.preload('avatarBg2.png');
   game.rootScene.backgroundColor = 'gray';
   game.keybind(32, 'sp'); // スペースキーをspとして登録
+  game.fps = 30;
 
   game.onload = function () {
     // 物理エンジン使用宣言
@@ -15,12 +17,12 @@ window.onload = function () {
       world.step(game.fps);
     }
 
-    // 床オブジェクト
-    let Floor = Class.create(PhyBoxSprite, {
+    // ボムオブジェクト
+    let Bomb = Class.create(Sprite, {
       initialize: function (x, y) {
-        PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false); // PhyBoxSprite(幅, 高さ, 動作モード, 密度, 摩擦力, 反発力, フラグ)
+        Sprite.call(this, 16, 16);
         this.image = game.assets['map2.png'];
-        this.frame = 0;
+        this.frame = 11;
         this.x = x;
         this.y = y;
         game.rootScene.addChild(this);
@@ -28,84 +30,65 @@ window.onload = function () {
         this.on('enterframe', () => {
           this.x -= 5;
 
-          if (this.x < 0) {
-            this.x = 1280
+          if (this.x < -50) {
+            this.x = 1280;
           }
         });
       }
     });
 
-    let floors = [];
-    for (let i = 0; i < 20; ++i) {
-      floors[i] = new Floor(i * 16, 200);
-    }
-    for (let i = 20; i < 24; ++i) {
-      floors[i] = new Floor(i * 16 + 80, 200);
-    }
-    for (let i = 24; i < 28; ++i) {
-      floors[i] = new Floor(i * 16 + 160, 200);
-    }
-    for (let i = 28; i < 32; ++i) {
-      floors[i] = new Floor(i * 16 + 200, 200);
-    }
-    for (let i = 32; i < 38; ++i) {
-      floors[i] = new Floor(i * 16 + 280, 240);
-    }
-    for (let i = 38; i < 44; ++i) {
-      floors[i] = new Floor(i * 16 + 360, 240);
-    }
-    for (let i = 44; i < 48; ++i) {
-      floors[i] = new Floor(i * 16 + 460, 240);
-    }
+    // 天井オブジェクト
+    let Roof = Class.create(PhyBoxSprite, {
+      initialize: function(x, y) {
+        PhyBoxSprite.call(this, 16, 16, enchant.box2d.STATIC_SPRITE, 0, 1.0, 0, false);
+        this.image = game.assets['map2.png'];
+        this.frame = 0;
+        this.x = x;
+        this.y = y;
+        game.rootScene.addChild(this);
+      }
+    });
 
     // くまオブジェクト
-    let bear = new PhyBoxSprite(32, 32, enchant.box2d.DYNAMIC_SPRITE, 5.0, 0.5, 0, true);
+    let bear = new PhyBoxSprite(32, 32, enchant.box2d.DYNAMIC_SPRITE, 5.0, 0.5, 0, true); // PhyBoxSprite(幅, 高さ, 動作モード, 密度, 摩擦力, 反発力, フラグ)
     bear.image = game.assets['chara1.png'];
     bear.x = 30;
     bear.y = 160;
-    bear.frame = 4;
-
+    bear.frame = 15;
+    
     bear.on('enterframe', () => {
       labelTime.text = parseInt(labelTime.text) + 5;
       bear.x = 80;
       bear.angle = 0; // くまが回転しないように固定
-      bear.onFloor = false;
+      bear.frame = bear.age % 3 + 15;
 
-      for (let i = 0; i < floors.length; ++i) {
-        if (bear.within(floors[i], 25)) {
-          bear.onFloor = true;
-          break;
-        }
+      // ボムと衝突でゲームオーバー
+      for (let i = 0; i < bombs.length; ++i) {
+        if (bear.within(bombs[i], 30)) {
+          game.pushScene(gameOverScene);
+          game.stop();
+          }
       }
       // スペースキーでジャンプ
-      if (game.input.sp && bear.onFloor) {
+      if (game.input.sp) {
         bear.applyImpulse(new b2Vec2(0, -15));
       }
-
+      
       // クリックでジャンプ
       game.rootScene.on('touchstart', function () {
-        bear.applyImpulse(new b2Vec2(0, -0.2));
+        bear.applyImpulse(new b2Vec2(0, -1));
       });
-
+      
       if (bear.y > 360) {
         game.pushScene(gameOverScene);
         game.stop();
       }
-
-      // 敵との当たり判定(within)
-      if (bear.intersect(floors[5])) {
-        labelHit.text = "Hit!";
-        game.pushScene(gameOverScene);
-        game.stop();
-      } else {
-        labelHit.text = "remote";
-      }
     });
-
+    
     // ゲームオーバーシーン
     let gameOverScene = new Scene();
     gameOverScene.backgroundColor = 'black';
-
+    
     // ゲームオーバーテキスト
     // let labelGameover = new Label();
     // labelGameover.x = 300;
@@ -113,7 +96,35 @@ window.onload = function () {
     // labelGameover.font = "24px Arial"
     // labelGameover.text = "Game Over!¥nPush Space key!";
     // game.gameOverScene.addChild(labelGameover);
-
+    
+    // 背景
+    let back1 = new Sprite(320, 50);
+    back1.image = game.assets['avatarBg2.png'];
+    back1.x = 880;
+    back1.y = 135;
+    back1.scaleX = 32 / 5;
+    back1.scaleY = 32 / 5;
+    back1.frame = 3;
+    back1.on('enterframe', () => {
+      back1.x -= 5;
+      if (back1.x < -800) {
+        back1.x = 2400;
+      }
+    });
+    let back2 = new Sprite(320, 50);
+    back2.image = game.assets['avatarBg2.png'];
+    back2.x = 2400;
+    back2.y = 135;
+    back2.scaleX = 32 / 5;
+    back2.scaleY = 32 / 5;
+    back2.frame = 3;
+    back2.on('enterframe', () => {
+      back2.x -= 5;
+      if (back2.x < -800) {
+        back2.x = 2400;
+      }
+    });
+    
     // 経過時間を表示するラベル
     let labelTime = new Label();
     labelTime.x = 250;
@@ -124,7 +135,7 @@ window.onload = function () {
     labelTime.on('enterframe', () => {
       //labelTime.text = (game.frame / game.fps).toFixed(2);
     });
-
+    
     // 当たり判定を表示するラベル
     let labelHit = new Label();
     labelHit.x = 250;
@@ -132,10 +143,22 @@ window.onload = function () {
     labelHit.color = 'red';
     labelHit.font = '14px Arial';
     labelHit.text = 'hit';
-
+    
+    game.rootScene.addChild(back1);
+    game.rootScene.addChild(back2);
     game.rootScene.addChild(bear);
     game.rootScene.addChild(labelTime);
     game.rootScene.addChild(labelHit);
+    let bombs = [];
+    for (let i = 0; i < 20; ++i) {
+      bombs[i] = new Bomb(rand(1280) + 250, rand(320));
+    }
+    // 天井
+    let roofs = [];
+    for (let i = 0; i < 10; ++i) {
+      roofs[i] = new Roof(i * 16, -16);
+    }
+
   }
   game.start();
 };
